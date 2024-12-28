bfcfeba6afcbd41ca12acc63e8678c05379508e7
sjam21 <srujanacharya69@gmail.com>
2024-12-28 20:01:00 +0530

    lsp phase-1

--- lua/suru/lazy.lua
+++ lua/suru/lazy.lua
@@ -124,6 +124,64 @@ require("lazy").setup({
         config = function()
             vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
         end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/nvim-cmp",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+            "j-hui/fidget.nvim",
+        },
+
+        config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    -- "lua_ls", "bashls", "clangd", "cmake", "dockerls", "gopls", "eslint", "jsonls", "grammerly", "ts_ls"
+                    "lua_ls", "bashls", "cmake", "eslint", "jsonls", "ts_ls"
+                },
+                handler = {
+                    function(server_name)
+                        print("setting up..", server_name)
+                        require("lspconfig")[server_name].setup {}
+                    end
+                }
+            })
+
+            local cmp = require('cmp')
+            cmp.setup({
+                snippet = {
+                    -- REQUIRED - you must specify a snippet engine
+                    expand = function(args)
+                        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    -- { name = 'vsnip' }, -- For vsnip users.
+                    { name = 'luasnip' }, -- For luasnip users.
+                    -- { name = 'ultisnips' }, -- For ultisnips users.
+                    -- { name = 'snippy' }, -- For snippy users.
+                }, {
+                    { name = 'buffer' },
+                })
+            })
+        end,
     }
 
     --"zenmode/folke"
